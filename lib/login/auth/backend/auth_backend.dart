ff5e75caa9a47881bbfc346d7e6cc9c0412f0a80
Mudaser <mudaserrahman7@gmail.com>
2026-07-07 23:05:38 +0500

    now deep linking backend is done

--- lib/login/auth/backend/auth_backend.dart
+++ lib/login/auth/backend/auth_backend.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:supabase_from_zero_to_end/login/auth/screen/home_screen.dart';
 
 class AuthBackend {
   final supabase = Supabase.instance.client;
+
+  Future<void> listenAuthentication(BuildContext context) async {
+    supabase.auth.onAuthStateChange.listen((event) {
+      final session = event.session;
+      if (session != null) {
+        final user = event.session!.user;
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => HomeScreen()),
+        );
+      }
+    });
+  }
+
   // now write code for signup
   Future<void> signUp(String email, String password) async {
     await supabase.auth.signUp(
